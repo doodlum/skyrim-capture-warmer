8289de16d06e950ef4dd7a702608bfc83a9faf47
doodlum <15017472+doodlum@users.noreply.github.com>
2026-08-22 18:16:30 +0100

    Add Skyrim 1.7.99 support with CommonLibSSE-NG

--- cmake/XSEPlugin.cmake
+++ cmake/XSEPlugin.cmake
@@ -1,3 +1,10 @@
+set(CommonLibPath "${CMAKE_SOURCE_DIR}/extern/CommonLibSSE-NG" CACHE PATH "Path to CommonLibSSE-NG")
+set(COMMONLIB_PREBUILT ON CACHE BOOL "Use prebuilt CommonLibSSE-NG" FORCE)
+set(COMMONLIB_PREBUILT_MULTICONFIG ON CACHE BOOL "Use prebuilt CommonLibSSE-NG with multi-config generators" FORCE)
+set(ENABLE_SKYRIM_SE ON CACHE BOOL "Enable Skyrim SE support" FORCE)
+set(ENABLE_SKYRIM_AE ON CACHE BOOL "Enable Skyrim AE support" FORCE)
+set(ENABLE_SKYRIM_VR ON CACHE BOOL "Enable Skyrim VR support" FORCE)
+
 add_library("${PROJECT_NAME}" SHARED)
 
 target_compile_features(
@@ -105,7 +112,7 @@ endif()
 
 find_package(directxtk CONFIG REQUIRED)
 
-find_package(CommonLibSSE REQUIRED)
+add_subdirectory(${CommonLibPath} CommonLibSSE EXCLUDE_FROM_ALL)
 
 target_link_libraries(
 	"${PROJECT_NAME}"
